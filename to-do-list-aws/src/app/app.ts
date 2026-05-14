14f9e6e6fa494dd24424049b39c41f810a3bf445
giancarlosierra <giancarlosierra2011@hotmail.com>
2026-05-13 19:06:32 -0500

    skeleton project

--- to-do-list-aws/src/app/app.ts
+++ to-do-list-aws/src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('to-do-list-aws');
+  task: string = '';
+  taskList: { id: number, name: string }[] = [];
+
+  addTask() {
+    if (this.task.trim()) {
+      const newTask = { id: Date.now(), name: this.task.trim() };
+      this.taskList.push(newTask);
+      this.task = '';
+      console.log(this.taskList);
+    }
+  }
+
+  deleteTask(id: number) {
+    this.taskList = this.taskList.filter(task => task.id !== id);
+  }
 }
