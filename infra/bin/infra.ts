acd85288975420b916d56bdd53dac4be5228e317
giancarlosierra <giancarlosierra2011@hotmail.com>
2026-08-30 17:36:01 -0500

    update cdk

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,12 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
-import { FrontendStack } from '../lib/frontend-stack';
+import { FrontendStack } from '../lib/infra-stack';
 
 const app = new cdk.App();
 
-new FrontendStack(app, 'AngularFrontendStack', {
+new FrontendStack(app, 'FrontendStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
+    region: 'us-east-1',
   },
   githubOwner: 'gsierra22',
   githubRepo: 'to-do-list-aws',
